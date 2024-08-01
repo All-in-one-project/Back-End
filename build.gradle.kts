66bc683fbdefcf70d750b9a10dd35aa046772229
nanami-tomoe <npnp123npnp123@gmail.com>
2024-08-01 21:30:27 +0900

    feat: Redis 의존성 주석 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -38,6 +37,10 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.104.Final:osx-aarch_64") // MacOS Silicon 라이브러리 누락 문제
+
+	// Spring Data Redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive") // WebFlux + Redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis") // Redis
 
 	// Mockito
 	testImplementation("org.mockito:mockito-core:3.11.2")
