3250ed147fb8d8f34853f6d002d3e030ce449645
nanami-tomoe <npnp123npnp123@gmail.com>
2024-08-20 01:12:24 +0900

    feat: 예비 수강 신청 관련 코드 주석 처리 - 이전 코드들 테스트를 위해 개발 중인 코드들은 주석처리함.

--- src/main/java/edu/allinone/sugang/domain/Basket.java
+++ src/main/java/edu/allinone/sugang/domain/Basket.java
@@ -1,51 +1,52 @@
-package edu.allinone.sugang.domain;
-
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(
-        name = "basket",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"student_id", "lecture_id"})
-        }
-)
-public class Basket {
-    /* -------------------------------------------- */
-    /* -------------- Default Column -------------- */
-    /* -------------------------------------------- */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    /* -------------------------------------------- */
-    /* ------------ Information Column ------------ */
-    /* -------------------------------------------- */
-    @Column(name = "basket_max_credits", nullable = false)
-    private Integer basketMaxCredits;
-
-    /* -------------------------------------------- */
-    /* -------------- Relation Column ------------- */
-    /* -------------------------------------------- */
-    @ManyToOne
-    @JoinColumn(name = "student_id", nullable = false)
-    private Student student;
-
-    @ManyToOne
-    @JoinColumn(name = "lecture_id", nullable = false)
-    private Lecture lecture;
-
-    /* -------------------------------------------- */
-    /* ----------------- Functions ---------------- */
-    /* -------------------------------------------- */
-    @Builder
-    public Basket(Student student, Lecture lecture) {
-        this.student = student;
-        this.lecture = lecture;
-    }
-}
+//package edu.allinone.sugang.domain;
+//
+//import jakarta.persistence.*;
+//import lombok.AccessLevel;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Table(
+//        name = "basket",
+//        uniqueConstraints = {
+//                @UniqueConstraint(columnNames = {"student_id", "lecture_id"})
+//        }
+//)
+//public class Basket {
+//    /* -------------------------------------------- */
+//    /* -------------- Default Column -------------- */
+//    /* -------------------------------------------- */
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//
+//    /* -------------------------------------------- */
+//    /* ------------ Information Column ------------ */
+//    /* -------------------------------------------- */
+//    @Column(name = "basket_max_credits", nullable = false)
+//    private Integer basketMaxCredits;
+//
+//    /* -------------------------------------------- */
+//    /* -------------- Relation Column ------------- */
+//    /* -------------------------------------------- */
+//    @ManyToOne
+//    @JoinColumn(name = "student_id", nullable = false)
+//    private Student student;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "lecture_id", nullable = false)
+//    private Lecture lecture;
+//
+//    /* -------------------------------------------- */
+//    /* ----------------- Functions ---------------- */
+//    /* -------------------------------------------- */
+//    @Builder
+//    public Basket(Student student, Lecture lecture, Integer basketMaxCredits) {
+//        this.student = student;
+//        this.lecture = lecture;
+//        this.basketMaxCredits = basketMaxCredits;
+//    }
+//}
