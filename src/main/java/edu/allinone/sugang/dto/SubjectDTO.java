295294ceb3781aad98e56cff263b3b50fde707d3
parksomii <somi020605@gmail.com>
2024-08-28 16:00:11 +0900

    feat: 과목조회 반환값 수정

--- src/main/java/edu/allinone/sugang/dto/SubjectDTO.java
+++ src/main/java/edu/allinone/sugang/dto/SubjectDTO.java
@@ -4,11 +4,9 @@ import lombok.Setter;
 
 @Getter @Setter
 public class SubjectDTO {
-    private Integer id;
     private String subjectName;
     private String subjectDivision;
-    private String targetGrade;
-    private Integer hoursPerWeek;
-    private Integer credit;
-    private Integer departmentId;
+    private String subjectCode;
+    private String professorName;
+    private String lectureTime;
 }
