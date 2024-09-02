4b30e2fe21a6b2d0822d9866998708ae7ec29940
yezanee <rbfrk02@naver.com>
2024-09-03 00:52:44 +0900

    로그인 반환값 수정

--- src/main/java/edu/allinone/sugang/service/StudentLoginService.java
+++ src/main/java/edu/allinone/sugang/service/StudentLoginService.java
@@ -24,7 +24,6 @@ public class StudentLoginService {
     private final StudentRepository studentRepository;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final JwtTokenProvider jwtTokenProvider;
-    private Department department;
 
     @Transactional
     public StudentDTO signIn(String username, String password) {
@@ -46,7 +45,7 @@ public class StudentLoginService {
         return new StudentDTO(
                 student.getStudentNumber(),
                 student.getId(),
-                department.getDepartmentName(),
+                student.getDepartment().getDepartmentName(),
                 student.getStudentName(),
                 student.getGrade(),
                 student.getMaxCredits(),
