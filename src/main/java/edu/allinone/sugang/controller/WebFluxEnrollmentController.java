4153e359eada4fd0b235e97b3c00a8c8c3c64ea4
parksomii <somi020605@gmail.com>
2024-09-11 01:02:47 +0900

    fix: lectureId 삭제

--- src/main/java/edu/allinone/sugang/controller/WebFluxEnrollmentController.java
+++ src/main/java/edu/allinone/sugang/controller/WebFluxEnrollmentController.java
@@ -19,13 +19,12 @@ public class WebFluxEnrollmentController {
      * 학생ID와 과목ID를 입력받고 수강 신청 대기열에 추가
      *
      * @param studentId 학생 ID
-     * @param lectureId 과목 ID
      * @return Mono<ResponseDTO<?>> 대기열 추가 결과
      */
     @PostMapping("/enqueue")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<ResponseDTO<EnqueueResponseDTO>> enqueue(@RequestParam Integer studentId, @RequestParam Integer lectureId) {
-        return enrollmentWebFluxService.enqueue(studentId) // 두 개의 파라미터 사용
+    public Mono<ResponseDTO<EnqueueResponseDTO>> enqueue(@RequestParam Integer studentId) {
+        return enrollmentWebFluxService.enqueue(studentId)
                 .onErrorResume(e -> Mono.just(new ResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "대기열 추가 중 오류가 발생했습니다.")));
     }
 
@@ -37,7 +36,7 @@ public class WebFluxEnrollmentController {
      */
     @GetMapping("/waiting-time")
     public Mono<ResponseDTO<Long>> getWaitingTime(@RequestParam Integer studentId) {
-        return enrollmentWebFluxService.getWaitingTime(studentId) // studentId와 lectureId 모두 전달
+        return enrollmentWebFluxService.getWaitingTime(studentId) // studentId 전달
                 .map(waitingTime -> new ResponseDTO<>(HttpStatus.OK.value(), "예상 대기시간", waitingTime))
                 .onErrorResume(e -> Mono.just(new ResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "대기시간 조회 중 오류가 발생했습니다.")));
     }
@@ -46,11 +45,10 @@ public class WebFluxEnrollmentController {
      * 진입 가능 여부 조회
      *
      * @param studentId 학생 ID
-     * @param lectureId 과목 ID
      * @return Mono<ResponseDTO<Boolean>> 진입 가능 여부 응답
      */
     @GetMapping("/can-enter/{studentId}/{lectureId}")
-    public Mono<ResponseDTO<Boolean>> canEnter(@PathVariable Integer studentId, @PathVariable Integer lectureId) {
+    public Mono<ResponseDTO<Boolean>> canEnter(@PathVariable Integer studentId) {
         return enrollmentWebFluxService.canEnter(studentId) // 두 개의 파라미터 전달
                 .flatMap(canEnter -> Mono.just(new ResponseDTO<>(HttpStatus.OK.value(), canEnter ? "수강신청이 가능합니다." : "아직 대기중입니다.", canEnter)))
                 .onErrorResume(e -> Mono.just(new ResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "진입 가능 여부 조회 중 오류가 발생했습니다.")));
